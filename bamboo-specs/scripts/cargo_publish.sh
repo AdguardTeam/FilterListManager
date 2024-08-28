683f453df03d184293dcd9faefcefdd9c6dabf4a
Maxim Grupper <m.grupper@adguard.com>
2024-08-28 14:56:17 +0300

    Pull request 66: Fix publish script flow when version is not found
    Merge in ADGUARD-CORE-LIBS/filter-list-manager from fix/publish_2 to master
    
    Squashed commit of the following:
    
    commit 0f759650f8eb398f8357a6c7140a0804fffe10e4
    Author: Maxim Grupper <m.grupper@adguard.com>
    Date:   Wed Aug 28 10:47:34 2024 +0400
    
        fix

--- bamboo-specs/scripts/cargo_publish.sh
+++ bamboo-specs/scripts/cargo_publish.sh
@@ -23,6 +23,9 @@ fi
 ADGUARD_FLM_VERSION=$(sed -ne 's/^ *version *= *\"\(.*\)\"/\1/p' crates/filter-list-manager/Cargo.toml)
 FFI_VERSION=$(sed -ne 's/^ *version *= *\"\(.*\)\"/\1/p' crates/ffi/Cargo.toml)
 
+echo "ADGUARD_FLM_VERSION: $ADGUARD_FLM_VERSION"
+echo "FFI_VERSION: $FFI_VERSION"
+
 # Login to the registry
 echo "Logging in to the registry..."
 # shellcheck disable=SC2154
@@ -30,7 +33,7 @@ cargo login ${bamboo_cargoPassword}
 
 # Check if the 'adguard-flm' version is already published
 echo "Checking if the 'adguard-flm' version is already published..."
-FOUND=$(cargo search adguard-flm | grep "adguard-flm = \"$ADGUARD_FLM_VERSION\"")
+FOUND=$(cargo search adguard-flm | grep "adguard-flm = \"$ADGUARD_FLM_VERSION\"" || true)
 
 if [ -z "$FOUND" ]; then
   echo "The 'adguard-flm' version is not published yet!"
@@ -45,14 +48,14 @@ if [ -z "$FOUND" ]; then
   # Wait for the 'adguard-flm' version to be available in the registry
   CRATE_NAME="adguard-flm"
   echo "Waiting for the '$CRATE_NAME' version to be available in the registry..."
-  FOUND=$(cargo search $CRATE_NAME | grep "$CRATE_NAME = \"$ADGUARD_FLM_VERSION\"")
+  FOUND=$(cargo search $CRATE_NAME | grep "$CRATE_NAME = \"$ADGUARD_FLM_VERSION\"" || true)
   ATTEMPT_COUNT=0
   ATTEMPT_LIMIT=20
   SLEEP_TIME=10
 
   while [ -z "$FOUND" ] && [ $ATTEMPT_COUNT -lt $ATTEMPT_LIMIT ]; do
     sleep $SLEEP_TIME
-    FOUND=$(cargo search $CRATE_NAME | grep "$CRATE_NAME = \"$ADGUARD_FLM_VERSION\"")
+    FOUND=$(cargo search $CRATE_NAME | grep "$CRATE_NAME = \"$ADGUARD_FLM_VERSION\"" || true)
     ATTEMPT_COUNT=$((ATTEMPT_COUNT+1))
   done
   if [ -z "$FOUND" ]; then
@@ -65,7 +68,7 @@ fi
 
 # Check if the 'ffi' crate is already published
 echo "Checking if the 'ffi' crate is already published..."
-FFI_FOUND=$(cargo search ffi | grep "ffi = \"$FFI_VERSION\"")
+FFI_FOUND=$(cargo search ffi | grep "ffi = \"$FFI_VERSION\"" || true)
 
 if [ -z "$FFI_FOUND" ]; then
   echo "The 'ffi' crate is not published yet!"
@@ -78,7 +81,7 @@ if [ -z "$FFI_FOUND" ]; then
   git config user.email "Bamboo"
 
   git add crates/ffi/Cargo.toml
-  git commit
+  git commit -m "Update adguard-flm version in ffi crate"
 
   # Publish the 'ffi' crate
   echo "Publishing 'ffi' crate..."
