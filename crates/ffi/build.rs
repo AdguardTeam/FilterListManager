4b8a5f77da3a03235db5f66ccbbc7160a4ac4eeb
Mikhail Berdnikov <m.berdnikov@adguard.com>
2024-12-19 18:54:38 +0200

    PR review 2

--- crates/ffi/build.rs
+++ crates/ffi/build.rs
@@ -3,4 +3,8 @@ use uniffi;
 fn main() {
     // Build uniffi
     uniffi::generate_scaffolding("src/flm_ffi.udl").unwrap();
+    
+    if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        windres::Build::new().compile("AGWinFLM.rc").unwrap();
+    }
 }
