99afef6779dccda389d80448cd74011bfff7c6b3
Bamboo <Bamboo>
2025-02-10 14:19:12 +0200

    AdGuardFLM for SwiftPM 1.3.5

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.4
+import PackageDescription
+
+let package = Package(
+  name: "AdGuardFLM",
+  platforms: [
+    .iOS("11.2"), .macOS("10.15")
+  ],
+  products: [
+    .library(name: "AdGuardFLMLib", targets: ["AdGuardFLMLib"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.28.2")
+  ],
+  targets: [
+    .target(
+      name: "AdGuardFLMLib",
+      dependencies: [
+        .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+        .target(name: "AdGuardFLM")
+      ]
+    ),
+    .binaryTarget(
+      name: "AdGuardFLM",
+      url: "https://github.com/AdguardTeam/FilterListManager/releases/download/v1.3.5@swift-5/AdGuardFLM-1.3.5.zip",
+      checksum: "adbc6d0944ce88f8ddc5592c8253b2679943e30cc1fccdf80842ac0a05f54ca9"
+    ),
+  ]
+)
+
